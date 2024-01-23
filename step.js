542512bb4cb28246aac2d208268d2f70bcf1f3f2
Joshua Graham <joshuareynoldsgraham@gmail.com>
2024-01-22 19:13:50 -0600

    create a function that creates 'steps'

---
+++ step.js
@@ -0,0 +1,22 @@
+/** Steps
+ * Write a function called printSteps that takes a
+ * number argument n.  It should print steps that
+ * are n levels high, with appropriate width using
+ * the # character to build the steps outward.
+ */
+
+function printSteps(n) {
+    for (let height = 1; height <= n; height++) {
+        let step = '';
+
+        for (let width = 1; width<= n; width++) {
+            if (width <= height) {
+                step += '#';
+            } else {
+                step += ' ';
+            }
+        }
+
+        console.log(step)
+    }
+}
