055a0bed4b16515cde2d537003c5571721eef166
Joshua Graham <joshuareynoldsgraham@gmail.com>
2024-01-25 14:50:37 -0600

    function to create a pyramid

---
+++ pyramid.js
@@ -0,0 +1,21 @@
+/**
+ * Write a function called pyramid that takes a number
+ * argument, n, and prints a pyramid composed of the
+ * "#" character.
+ */
+
+function pyramid(n) {
+    for (let i = 1; i <= n; i++) {
+        let row = '';
+
+        for (let j = 1; j <= n - i; j++) {
+            row += ' ';
+        }
+
+        for (let k = 1; k <= i * 2 - 1; k++) {
+            row += '#';
+        }
+
+        console.log(row);
+    }
+}
